c7ed86a18adc4a86ef9ca9358070cd424f98e753
deynoris diaz <rd.gallardo@hotmail.com>
2022-03-04 23:18:17 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductsComponent,
+    CreateProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
